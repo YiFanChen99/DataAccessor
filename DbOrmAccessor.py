53a3f69bf3a44c909a9162acb053a27edef3ccdd
Ekko <pig211@gmail.com>
2018-09-08 01:23:20 +0800

    Use atomic to avoid change test-data on unittest.

--- DbOrmAccessor.py
+++ DbOrmAccessor.py
@@ -25,16 +25,19 @@ class _BaseModelTest(unittest.TestCase):
     def test_create_and_delect(self):
         self.assertEqual(2, _MockFlesh.select().count())
 
-        new_instance = _MockFlesh.create(count=0.4)
-        self.assertEqual(3, _MockFlesh.select().count())
-        new_instance.delete_instance()
-        self.assertEqual(2, _MockFlesh.select().count())
+        with db.atomic() as txn:
+            new_instance = _MockFlesh.create(count=0.4)
+            self.assertEqual(3, _MockFlesh.select().count())
+            new_instance.delete_instance()
+            self.assertEqual(2, _MockFlesh.select().count())
 
-        _MockFlesh.create(date="2018-07-20")
-        _MockFlesh.create(date="2018-07-20", count=0.9)
-        query = _MockFlesh.delete().where(_MockFlesh.date == "2018-07-20")
-        query.execute()
-        self.assertEqual(2, _MockFlesh.select().count())
+            _MockFlesh.create(date="2018-07-20")
+            _MockFlesh.create(date="2018-07-20", count=0.9)
+            query = _MockFlesh.delete().where(_MockFlesh.date == "2018-07-20")
+            query.execute()
+            self.assertEqual(2, _MockFlesh.select().count())
+
+            txn.rollback()
 
     def test_select_with_condition(self):
         self.assertEqual(1, _MockFlesh.select().where(_MockFlesh.count < 0.4).count())
